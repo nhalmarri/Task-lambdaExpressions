545d30a46a28ca2dd83567fa3a90161a93dda956
Nora <nhalmarri64@gmail.com>
2024-11-17 10:40:33 +0300

    Initial commit

---
+++ src/main/java/com/example/tasklambdaexpressions/TaskLambdaExpressionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasklambdaexpressions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskLambdaExpressionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskLambdaExpressionsApplication.class, args);
+    }
+
+}
